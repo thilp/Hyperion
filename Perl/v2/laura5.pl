3af4a4c0f8c51334fb37a39cfbe1d2f6389ae21a
Thibaut Le Page <thilp.is@gmail.com>
2011-08-16 11:53:06 +0200

    rangement des fonctions pour y voir plus clair

--- Perl/v2/laura5.pl
+++ Perl/v2/laura5.pl
@@ -20,6 +20,8 @@ use Data::Dumper;
 use Date::Calc qw(Mktime Localtime);
 require "edit.pl";
 
+# #############################################################################################################
+
 # CHARGEMENT DE LA BASE DE DONNÉES
 if (-e "lauraperl.dis") { open(BDD, "+<lauraperl.dis"); }
 else { open(BDD, "+>lauraperl.dis"); }
@@ -31,19 +33,189 @@ while ( <BDD> ) {
 close(BDD);
 # FIN CHARGEMENT
 
+# ################################################################################################################
+
+# FONCTIONS UTILITAIRES DE CALCUL
+
+sub unix_of_wtimestamp {
+# transforme un timestamp MediaWiki en valeur de temps Unix
+    return Mktime( $_[0] =~ m/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z/ );
+}
+sub wtimestamp_of_unix {
+# transforme une valeur de temps Unix en timestamp MediaWiki
+    my ($a,$m,$j,$h,$min,$s) = Localtime($_[0]);
+    $m = "0".$m if ( length($m) == 1 ); $j = "0".$j if ( length($j) == 1 ); $h = "0".$h if ( length($h) == 1 );
+    $min = "0".$min if ( length($min) == 1 ); $s = "0".$s if ( length($s) == 1 );
+    return $a."-".$m."-".$j."T".$h.":".$min.":".$s."Z";
+}
+sub jours_depuis {
+# calcule le nombre de jours écoulés entre aujourd’hui et le temps Unix donné
+    return int( (time() - $_[0]) / 86400 );
+}
+sub estIP {
+# renvoie 1 si le pseudo est une IP, 0 sinon
+    if ( my ($un,$deux,$trois,$quatre) = ($_[0] =~ m/(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})/) ) {
+	return 1 if ($un < 255 && $deux < 255 && $trois < 255 && $quatre < 255);
+	return 0;
+    }
+    return 0;
+}
+
+# FIN FONCTIONS UTILITAIRES DE CALCUL
+
+# ################################################################################################################
+
+# FONCTIONS DE RÉCUPÉRATION DEPUIS L’API
+
 sub getMembres_d1_groupe {
 # Récupère la liste des membres d’un groupe d’utilisateurs
     return (requeteAPI('action#=query#&list#=allusers#&aulimit#=5000#&augroup#='.$_[0]) =~ m/\bname="([^"]+)"/g);
 }
-my @administrateurs = getMembres_d1_groupe("sysop");
-my @autopatrol = getMembres_d1_groupe("autopatrol");
-my @patrouilleurs = getMembres_d1_groupe("patroller");
-
 sub getListe_balises {
 # Récupère la liste des balises AbuseFilter
     return (requeteAPI("action#=query#&list#=tags#&tglimit#=500") =~ m/\bname="([^"]+)"/g);
 }
+sub estScolaire {
+# renvoie 1 si la page de discussion du compte d’utilisateur mentionne qu’il est utilisé dans un cadre scolaire, 0 sinon,
+    if ( lectureArticle("Discussion utilisateur:".$_[0]) =~ m/\{\{ip scolaire\|/i ) {
+	return 1;
+    }
+    return 0;
+}
+sub getUser_infos {
+# renvoie l’âge, le nombre d’éditions et le statut le plus important (dans la hiérarchie de LAURA) de l’utilisateur dont le nom est donné
+    my $rep = requeteAPI("action#=query#&list#=allusers#&auprop#=groups|editcount|registration#&aulimit#=1#&aufrom#=".$_[0]);
+    my ($editcount,$registration) = ($rep =~ m/\beditcount="(\d+)" registration="([^"]+)"/);
+    my $age = jours_depuis(unix_of_wtimestamp($registration));
+    my $statut = "*";
+    if ( $rep =~ m/<groups>/ ) {
+	my @groupes = ( $rep =~ m#(?<=<g>)[^<]+(?=</g>)#g );
+	if ( "developer" ~~ @groupes ) { $statut = "developer"; }
+	elsif ( "bureaucrat" ~~ @groupes ) { $statut = "bureaucrat"; }
+ 	elsif ( "sysop" ~~ @groupes ) { $statut = "sysop"; }
+	elsif ( "abusefilter" ~~ @groupes ) { $statut = "abusefilter"; }
+	elsif ( "autopatrol" ~~ @groupes ) { $statut = "autopatrol"; }
+	elsif ( "patroller" ~~ @groupes ) { $statut = "patroller"; }
+	else { $statut = "autre"; }
+    }
+    return ($age,$editcount,$statut,$registration);
+}
+sub getNbre_blocages {
+# renvoie le nombre de blocages subis par un utilisateur
+# on compte le nombre de blocages subis (hormis les autoblocages) - le nombre de blocages annulés
+    # action=query&list=logevents&leaction=block/block&leprop=user&lelimit=500&letitle=Utilisateur:
+    my @countblocks =
+	( requeteAPI("action#=query#&list#=logevents#&leaction#=block/block#&leprop#=user#&lelimit#=500#&letitle#=Utilisateur:".$_[0]) =~ m/<item user="(?!$_[0]")/g );
+    my @countunblocks =
+	( requeteAPI("action#=query#&list#=logevents#&leaction#=block/unblock#&leprop#=user#&lelimit#=500#&letitle#=Utilisateur:".$_[0]) =~ m/<item user="(?!$_[0]")/g );
+    return (@countblocks - @countunblocks);
+}
+sub getNbre_balises {
+# renvoie le nombre de balises déclenchées par un utilisateur donné
+    my $compte = 0;
+    my @temp;
+    foreach $balise (@balises) {
+	@temp = ( requeteAPI("action#=query#&list#=usercontribs#&ucuser#=".$_[0]."#&uclimit#=5000#&ucprop#=#&uctag#=".$balise) =~ m/<item\b/g );
+	$compte += @temp;
+    }
+    return $compte;
+}
+sub getNbre_averts {
+# renvoie le nombre d’avertissements présents *actuellement* sur la page de discussion d’un utilisateur donné
+    my $texte = lectureArticle("Discussion utilisateur:".$_[0]);
+    my @nbre = ( $texte =~ m/\{\{averto-/g );
+    return scalar(@nbre);
+}
+
+# FIN FONCTIONS DE RÉCUPÉRATION DEPUIS L’API
+
+# ################################################################################################################
+
+# FONCTIONS D’INTERACTION AVEC LA BASE DE DONNÉES
+
+sub getListe_autres_ips {
+# renvoie la liste des autres ips appartenant au même masque /24 que l’IP donnée et répertoriées dans %bdd.
+    my $masque = ( $_[0] =~ s/(?<=\.)\d{1,2}$// );
+    my @ips = ( grep { /^$masque/ } keys %bdd );
+    for ( my $i=0; $i<@ips; $i++ ) {
+	delete $ips[$i] if ( $ips[$i] eq $_[0] );
+    }
+    return @ips;
+}
+sub getChaine_autres_ips {
+# renvoie le résultat de getAutres_ips() sous forme de chaîne de caractères.
+    return join(",",getListe_autres_ips($_[0]));
+}
+sub majUser_infos {
+# met à jour le couple (nom:valeur) d’un utilisateur donné : majUser_infos(utilisateur,nom,valeur);
+    $bdd{$_[0]} =~ s/$_[1]:[^;]+/$_[1]:$_[2]/;
+    return 1;
+}
+sub ecritUser_infos {
+# pour l’utilisateur donné en argument (0), écrit les informations passées en argument (1)
+    $bdd{$_[0]} = $_[1]; return 1;
+}
+sub rechercheUser_infos {
+# renvoie les informations de l’utilisateur donné en argument
+    return $bdd{$_[0]};
+}
+sub majBDD {
+# met à jour la base de données sauvegardée : écrit le contenu de %bdd dans le fichier lauraperl.dis
+    # copie de secours du fichier
+    open(OP,"|cp lauraperl.dis lauraperl.dis.sauv"); close(OP);
+    # écriture
+    open(BDD, ">lauraperl.dis");
+    foreach $cle (keys(%bdd)) {
+	print( BDD $cle.":".$bdd{$cle}."\n" );
+    }
+    close(BDD);
+    return 1;
+}
+sub nouvUser_infos {
+# crée le champ d’informations d’un nouvel utilisateur dont le nom est passé en argument
+    my ($age,$nbre_contribs,$statut,$registration) = getUser_infos($_[0]);
+    my $ip = estIP($_[0]);
+    my $nbre_balises = getNbre_balises($_[0]);
+    my $nbre_averts = getNbre_averts($_[0]);
+    my $nbre_blocages = getNbre_blocages($_[0]);
+    my $scolaire = estScolaire($_[0]);
+    if ( $ip ) { $ip .= ";aip:".getChaine_autres_ips($_[0]); }
+    my $note = calculNote_confiance_base($scolaire,$statut,$age,$nbre_contribs,$nbre_averts,$nbre_balises,$nbre_blocages);
+    $bdd{$_[0]} = "not:$note;edc:$nbre_contribs;reg:$registration;sta:$statut;iip:$ip;sco:$scolaire;nav:$nbre_averts;nbl:$nbre_blocages;nba:$nbre_balises;";
+    return 1;
+}
+
+# FIN FONCTIONS D’INTERACTION AVEC LA BASE DE DONNÉES
+
+# ################################################################################################################
+my @administrateurs = getMembres_d1_groupe("sysop");
+my @autopatrol = getMembres_d1_groupe("autopatrol");
+my @patrouilleurs = getMembres_d1_groupe("patroller");
 my @balises = getListe_balises();
+
+sub calculNote_confiance_base {
+    my ($scolaire,$statut,$age,$nbre_contribs,$nbre_averts,$nbre_balises,$nbre_blocages) = @_;
+    my $val_statut = 0;
+    $val_statut = 1 if ( $statut eq "patroller" );
+    $val_statut = 2 if ( $statut eq "autopatrol" );
+    $val_statut = 3 if ( $statut eq "sysop" or $statut eq "abusefilter" );
+    $val_statut = 4 if ( $statut eq "bureaucrat" );
+    $val_statut = 5 if ( $statut eq "developer" );
+    return
+	(1/(1+$scolaire))*(1+$val_statut)*(2*log(1+$age)+5*($nbre_contribs-$nbre_balises**2))/(7*((1+$nbre_blocages)**2+$nbre_averts));
+}
+
+foreach $nom ("thilp","Ptyx","Astirmays","Adoni273","Haroldetcoco","Giratina","Altshift") {
+    print("\nPour $nom :\n");
+    print("Âge, nombre de modifications, groupe principal :\n");
+    print Dumper(getUser_infos($nom));
+    print("Balises déclenchées : ");
+    print(getNbre_balises($nom)."\n");
+    print("Avertissements reçus : ");
+    print(getNbre_averts($nom)."\n");
+    print("Blocages subis : ");
+    print(getNbre_blocages($nom)."\n");
+}
 
 # LAURA doit suivre l’évolution de chaque contributeur en fonction de ses contributions :
 #   plus ses modifications sont annulées ou retravaillées, moins elle a confiance en lui ;
@@ -86,165 +258,3 @@ my @balises = getListe_balises();
 #
 #   (\*|bot|sysop|bureaucrat|patroller|autopatrol|developer|abusefilter)[,…];
 #               groupe majeur de l’utilisateur
-#   
-
-sub calculNote_confiance_base {
-    my ($scolaire,$statut,$age,$nbre_contribs,$nbre_averts,$nbre_balises,$nbre_blocages) = @_;
-    my $val_statut = 0;
-    $val_statut = 1 if ( $statut eq "patroller" );
-    $val_statut = 2 if ( $statut eq "autopatrol" );
-    $val_statut = 3 if ( $statut eq "sysop" or $statut eq "abusefilter" );
-    $val_statut = 4 if ( $statut eq "bureaucrat" );
-    $val_statut = 5 if ( $statut eq "developer" );
-    return
-	(1/(1+$scolaire))*(1+$val_statut)*(2*log(1+$age)+5*($nbre_contribs-$nbre_balises**2))/(7*((1+$nbre_blocages)**2+$nbre_averts));
-}
-
-sub unix_of_wtimestamp {
-# transforme un timestamp MediaWiki en valeur de temps Unix
-    return Mktime( $_[0] =~ m/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z/ );
-}
-sub wtimestamp_of_unix {
-# transforme une valeur de temps Unix en timestamp MediaWiki
-    my ($a,$m,$j,$h,$min,$s) = Localtime($_[0]);
-    $m = "0".$m if ( length($m) == 1 ); $j = "0".$j if ( length($j) == 1 ); $h = "0".$h if ( length($h) == 1 );
-    $min = "0".$min if ( length($min) == 1 ); $s = "0".$s if ( length($s) == 1 );
-    return $a."-".$m."-".$j."T".$h.":".$min.":".$s."Z";
-}
-sub jours_depuis {
-# calcule le nombre de jours écoulés entre aujourd’hui et le temps Unix donné
-    return int( (time() - $_[0]) / 86400 );
-}
-
-sub estIP {
-# renvoie 1 si le pseudo est une IP, 0 sinon
-    if ( my ($un,$deux,$trois,$quatre) = ($_[0] =~ m/(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})/) ) {
-	return 1 if ($un < 255 && $deux < 255 && $trois < 255 && $quatre < 255);
-	return 0;
-    }
-    return 0;
-}
-
-sub estScolaire {
-# renvoie 1 si la page de discussion du compte d’utilisateur mentionne qu’il est utilisé dans un cadre scolaire, 0 sinon,
-    if ( lectureArticle("Discussion utilisateur:".$_[0]) =~ m/\{\{ip scolaire\|/i ) {
-	return 1;
-    }
-    return 0;
-}
-
-sub getListe_autres_ips {
-# renvoie la liste des autres ips appartenant au même masque /24 que l’IP donnée et répertoriées dans %bdd.
-    my $masque = ( $_[0] =~ s/(?<=\.)\d{1,2}$// );
-    my @ips = ( grep { /^$masque/ } keys %bdd );
-    for ( my $i=0; $i<@ips; $i++ ) {
-	delete $ips[$i] if ( $ips[$i] eq $_[0] );
-    }
-    return @ips;
-}
-
-sub getChaine_autres_ips {
-# renvoie le résultat de getAutres_ips() sous forme de chaîne de caractères.
-    return join(",",getListe_autres_ips($_[0]));
-}
-
-sub getUser_infos {
-# renvoie l’âge, le nombre d’éditions et le statut le plus important (dans la hiérarchie de LAURA) de l’utilisateur dont le nom est donné
-    my $rep = requeteAPI("action#=query#&list#=allusers#&auprop#=groups|editcount|registration#&aulimit#=1#&aufrom#=".$_[0]);
-    my ($editcount,$registration) = ($rep =~ m/\beditcount="(\d+)" registration="([^"]+)"/);
-    my $age = jours_depuis(unix_of_wtimestamp($registration));
-    my $statut = "*";
-    if ( $rep =~ m/<groups>/ ) {
-	my @groupes = ( $rep =~ m#(?<=<g>)[^<]+(?=</g>)#g );
-	if ( "developer" ~~ @groupes ) { $statut = "developer"; }
-	elsif ( "bureaucrat" ~~ @groupes ) { $statut = "bureaucrat"; }
- 	elsif ( "sysop" ~~ @groupes ) { $statut = "sysop"; }
-	elsif ( "abusefilter" ~~ @groupes ) { $statut = "abusefilter"; }
-	elsif ( "autopatrol" ~~ @groupes ) { $statut = "autopatrol"; }
-	elsif ( "patroller" ~~ @groupes ) { $statut = "patroller"; }
-	else { $statut = "autre"; }
-    }
-    return ($age,$editcount,$statut,$registration);
-}
-
-sub majUser_infos {
-# met à jour le couple (nom:valeur) d’un utilisateur donné : majUser_infos(utilisateur,nom,valeur);
-    $bdd{$_[0]} =~ s/$_[1]:[^;]+/$_[1]:$_[2]/;
-    return 1;
-}
-
-sub nouvUser_infos {
-# crée le champ d’informations d’un nouvel utilisateur dont le nom est passé en argument
-    my ($age,$nbre_contribs,$statut,$registration) = getUser_infos($_[0]);
-    my $ip = estIP($_[0]);
-    my $nbre_balises = getNbre_balises($_[0]);
-    my $nbre_averts = getNbre_averts($_[0]);
-    my $nbre_blocages = getNbre_blocages($_[0]);
-    my $scolaire = estScolaire($_[0]);
-    if ( $ip ) { $ip .= ";aip:".getAutres_ips($_[0]); }
-    my $note = calculNote_confiance_base($scolaire,$statut,$age,$nbre_contribs,$nbre_averts,$nbre_balises,$nbre_blocages);
-    $bdd{$_[0]} = "not:$note;edc:$nbre_contribs;reg:$registration;sta:$statut;iip:$ip;sco:$scolaire;nav:$nbre_averts;nbl:$nbre_blocages;nba:$nbre_balises;";
-    return 1;
-}
-
-sub ecritUser_infos {
-# pour l’utilisateur donné en argument (0), écrit les informations passées en argument (1)
-    $bdd{$_[0]} = $_[1]; return 1;
-}
-sub rechercheUser_infos {
-# renvoie les informations de l’utilisateur donné en argument
-    return $bdd{$_[0]};
-}
-sub majBDD {
-# met à jour la base de données sauvegardée : écrit le contenu de %bdd dans le fichier lauraperl.dis
-    # copie de secours du fichier
-    open(OP,"|cp lauraperl.dis lauraperl.dis.sauv"); close(OP);
-    # écriture
-    open(BDD, ">lauraperl.dis");
-    foreach $cle (keys(%bdd)) {
-	print( BDD $cle.":".$bdd{$cle}."\n" );
-    }
-    close(BDD);
-    return 1;
-}
-
-sub getNbre_blocages {
-# renvoie le nombre de blocages subis par un utilisateur
-# on compte le nombre de blocages subis (hormis les autoblocages) - le nombre de blocages annulés
-    # action=query&list=logevents&leaction=block/block&leprop=user&lelimit=500&letitle=Utilisateur:
-    my @countblocks =
-	( requeteAPI("action#=query#&list#=logevents#&leaction#=block/block#&leprop#=user#&lelimit#=500#&letitle#=Utilisateur:".$_[0]) =~ m/<item user="(?!$_[0]")/g );
-    my @countunblocks =
-	( requeteAPI("action#=query#&list#=logevents#&leaction#=block/unblock#&leprop#=user#&lelimit#=500#&letitle#=Utilisateur:".$_[0]) =~ m/<item user="(?!$_[0]")/g );
-    return (@countblocks - @countunblocks);
-}
-
-sub getNbre_balises {
-# renvoie le nombre de balises déclenchées par un utilisateur donné
-    my $compte = 0;
-    my @temp;
-    foreach $balise (@balises) {
-	@temp = ( requeteAPI("action#=query#&list#=usercontribs#&ucuser#=".$_[0]."#&uclimit#=5000#&ucprop#=#&uctag#=".$balise) =~ m/<item\b/g );
-	$compte += @temp;
-    }
-    return $compte;
-}
-
-sub getNbre_averts {
-# renvoie le nombre d’avertissements présents *actuellement* sur la page de discussion d’un utilisateur donné
-    my $texte = lectureArticle("Discussion utilisateur:".$_[0]);
-    my @nbre = ( $texte =~ m/\{\{averto-/g );
-    return scalar(@nbre);
-}
-
-foreach $nom ("thilp","Ptyx","Astirmays","Adoni273","Haroldetcoco","Giratina","Altshift") {
-    print("\nPour $nom :\n");
-    print("Âge, nombre de modifications, groupe principal :\n");
-    print Dumper(getUser_infos($nom));
-    print("Balises déclenchées : ");
-    print(getNbre_balises($nom)."\n");
-    print("Avertissements reçus : ");
-    print(getNbre_averts($nom)."\n");
-    print("Blocages subis : ");
-    print(getNbre_blocages($nom)."\n");
-}
